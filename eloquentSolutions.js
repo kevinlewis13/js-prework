813dd9eec2bab77c5fe88911b46dd620569a5910
Kevin Lewis <kevinlewis13@gmail.com>
2015-04-26 19:00:52 -0700

    first solution

--- eloquentSolutions.js
+++ eloquentSolutions.js
@@ -6,3 +6,13 @@ var arrays = [[1, 2, 3], [4, 5], [6]];
 
 //my code here.
 
+var reducedArray = arrays.reduce(function(element, nextElement) {
+  return element.concat(nextElement);
+});
+
+console.log(reducedArray);
+
+// -> should be [1, 2, 3, 4, 5, 6]
+
+//Mother-Child Age Difference:
+
